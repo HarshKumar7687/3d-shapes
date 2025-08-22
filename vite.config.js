14697a6c6985eea3463f1fccdde1350af5b1da04
HarshKumar7687 <drharshkumar499@gmail.com>
2025-08-22 11:43:27 +0530

    Add dist folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/wireframe/", // 👈 IMPORTANT for GitHub Pages
+  base: "/3d-shapes/", // 👈 IMPORTANT for GitHub Pages
 });
